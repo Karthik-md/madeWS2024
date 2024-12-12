561a646d9145c004982721d5566a0baf1be9b4b6
Karthik M D <karthikmd9@gmail.com>
2024-12-12 10:59:37 +0100

    c

--- project/project.py
+++ project/project.py
@@ -12,6 +12,7 @@ def download_csv(url, folder_name, filename):
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
     
+    
     # Full path to save the file
     file_path = os.path.join(folder_name, filename)
     
