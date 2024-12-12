b48d445d90bc0cb4bbef344ad30b8d8eb61143a9
Karthik M D <karthikmd9@gmail.com>
2024-12-12 11:09:42 +0100

    d

--- project/test.sh
+++ project/test.sh
@@ -3,9 +3,6 @@
 # Run the tests
 python3 -m unittest discover -s project -p '*test.py'
 
-# Execute the data pipeline
-python3 project/project.py
-
 # Validate that the output files exist
 if [ -f "data/url1.csv" ] && [ -f "data.db" ]; then
     echo "System test passed: Output files exist."
